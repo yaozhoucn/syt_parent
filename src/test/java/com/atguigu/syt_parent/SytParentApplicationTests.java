8f70bcdfff4efe2cc51be78209c6918a94a5dbda
HANG <wangxiao_h@126.com>
2022-03-21 23:16:24 +0800

    Initial commit

---
+++ src/test/java/com/atguigu/syt_parent/SytParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.syt_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SytParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
