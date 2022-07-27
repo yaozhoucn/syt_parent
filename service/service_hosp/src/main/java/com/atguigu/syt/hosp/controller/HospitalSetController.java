544114bcad63ea35b42986a6618c69b7bd17590f
hang <wangxiao_h@126.com>
2022-07-27 09:45:57 +0800

    redis cache

--- service/service_hosp/src/main/java/com/atguigu/syt/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/syt/hosp/controller/HospitalSetController.java
@@ -11,7 +11,6 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
